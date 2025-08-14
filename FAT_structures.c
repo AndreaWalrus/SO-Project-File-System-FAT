3984498b378f9ecb40b9d07e0281eb6bcb1e730f
andrea nardone <anprocessing@gmail.com>
2025-08-14 21:28:02 +0200

    Refactored FAT structure and functions

--- FAT_structures.c
+++ FAT_structures.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 #include "FAT_structures.h"
 
+
 int init_fat(char* buffer){
     if (buffer == NULL) {
         return -1;
     }
     FATEntry fat = (FATEntry)buffer;
-    for (int i = 0; i < BLOCKS_AVAILABLE; i++) {
-        fat->blocks[i] = -1;
+    for (int i = 0; i < FAT_SIZE; i++) {
+        fat[i] = FAT_RSVD; // Set the first blocks of the FAT to reserved for the FAT itself
     }
-    fat->free_blocks = BLOCKS_NUM-((int)(BLOCKS_NUM*4)/BLOCK_SIZE);
+    for (int i = FAT_SIZE; i < BLOCKS_AVAILABLE; i++) {
+        fat[i] = FAT_FREE; // Set all the remaining blocks as free
+    }
     return 0;  
 }
 
-int find_free_block(FATEntry fat) {
-    for (int i = 0; i < BLOCKS_AVAILABLE; i++) {
-        if (fat->blocks[i] == -1) {
+fat_entry_t find_free_block(FATEntry fat) {
+    for (int i = 0; i < BLOCKS_NUM; i++) {
+        if (fat[i] == FAT_FREE) {
             return i;
         }
     }
     return -1;
 }
 
-int allocate_block(FATEntry fat, unsigned int start_block) {
-    int next_block = find_free_block(fat);
+fat_entry_t allocate_block(FATEntry fat, fat_entry_t start_block) {
+    fat_entry_t next_block = find_free_block(fat);
     if (next_block == -1) {
         return -1;
     }
-    fat->blocks[start_block] = next_block;
-    fat->free_blocks--;
+    fat[start_block] = next_block;
+    fat[next_block] = FAT_EOC;
     return next_block;
 }
 
-int free_block(FATEntry fat, unsigned int block_index) {
-    if (block_index >= BLOCKS_NUM || fat->blocks[block_index] == -1) {
+fat_entry_t free_block(FATEntry fat, fat_entry_t block_index) {
+    if (block_index >= BLOCKS_NUM || fat[block_index] == FAT_FREE || fat[block_index] == FAT_RSVD) {
         return -1; 
     }
-    unsigned int next_block = fat->blocks[block_index];
-    fat->blocks[block_index] = -1;
-    fat->free_blocks++;
+    fat_entry_t next_block = fat[block_index];
+    fat[block_index] = FAT_FREE;
     return next_block;
 }
 
@@ -47,34 +49,32 @@ int createFile(const char* name, char* buffer) {
         return -1;
     }
     FATEntry fat = (FATEntry)buffer;
-    unsigned int start_block = find_free_block(fat);
+    fat_entry_t start_block = find_free_block(fat);
     if (start_block == -1) {
         return -1;
     }
-    FileEntry file = (FileEntry)(buffer + FAT_SIZE + (start_block * BLOCK_SIZE));
+    unsigned int offset = start_block * BLOCK_SIZE;
+    FileEntry file = (FileEntry)(buffer + offset);
     memcpy(file->name, name, 32);
     file->start_block = start_block;
     file->size = 0;
     file->is_directory = 0;
 
-    fat->blocks[start_block] = BLOCKS_AVAILABLE;
-    fat->free_blocks--;
+    fat[start_block] = FAT_EOC;
     return 0;
 }
 
-void eraseFATChain(FATEntry fat, unsigned int start_block) {
-    unsigned int current_block = start_block;
-    while (current_block != -1) {
-        if(current_block == BLOCKS_AVAILABLE)
-            fat->blocks[current_block] = -1;
-            fat->free_blocks++;
-            break;
-        unsigned int next_block = fat->blocks[current_block];
-        fat->blocks[current_block] = -1;
-        fat->free_blocks++;
-        current_block = next_block;
+void eraseFATChain(FATEntry fat, fat_entry_t start_block) {
+    while (start_block != FAT_EOC ) {
+        if(start_block == FAT_RSVD)
+            return;
+        fat_entry_t next_block = fat[start_block];
+        fat[start_block] = FAT_FREE;
+        start_block = next_block;
     }
-
+    if(start_block == FAT_RSVD)
+            return;
+    fat[start_block] = FAT_FREE;
 }
 
 int eraseFile(FileEntry file, char* buffer) {
--- FAT_structures.h
+++ FAT_structures.h
@@ -1,27 +1,26 @@
-#define BLOCK_SIZE 512 // Blocks of 512 bytes
-#define BLOCKS_NUM 1024 // Total number of blocks and FAT entries
-#define FAT_SIZE (int)(BLOCKS_NUM * 4) / BLOCK_SIZE // FAT size in blocks
-#define BLOCKS_AVAILABLE BLOCKS_NUM - FAT_SIZE // Number of blocks available for files and directories
-
 #pragma once
 
-// FAT structure size in buffer: [(BLOCKS_NUM-(BLOCKS_NUM * 4)/BLOCK_SIZE) * 4] + 4 bytes, so the FAT occupies the first (BLOCKS_NUM * 4)/BLOCK_SIZE blocks of the buffer
-// eg. with BLOCKS_NUM = 1024, BLOCK_SIZE = 512, the FAT occupies the first (1024*4)/512 = 8 blocks of the buffer
+#include <stdint.h>
 
-// Given that the FAT structure doesn't fill the last block completely, we can use the remaining space for a free_blocks counter, and the remaining bytes will be left empty
+#define BLOCK_SIZE 512 // Blocks of 512 bytes
+#define BLOCKS_NUM 1024 // Total number of blocks and FAT entries
 
-struct FAT{
-    int blocks[BLOCKS_AVAILABLE]; // -(BLOCKS_NUM*4)/BLOCK_SIZE to account for the first blocks reserved for the FAT itself
-    unsigned int free_blocks;
-};
+typedef int16_t fat_entry_t;
 
-// File structure size in buffer: 32 bytes for name + 4 bytes for start_block + 4 bytes for size + 1 byte for is_directory = 41 bytes
+#define FAT_SIZE (BLOCKS_NUM*sizeof(fat_entry_t)+BLOCK_SIZE-1) / BLOCK_SIZE // Number of blocks occupied by the FAT itself rounded up
+#define BLOCKS_AVAILABLE (BLOCKS_NUM - FAT_SIZE) // Number of blocks available for files and directories
+
+#define FAT_FREE (fat_entry_t)-1 // Free block flag
+#define FAT_EOC  (fat_entry_t)-2 // End of chain flag
+#define FAT_RSVD (fat_entry_t)-3 // Reserved blocks for the FAT itself
+
+// File structure size in buffer: 32 bytes for name + 4 bytes for start_block + 4 bytes for size + 1 byte for is_directory + 3 bytes of padding = 44 bytes
 
 struct File{
     char name[32];
-    unsigned int start_block; 
+    fat_entry_t start_block; 
     unsigned int size; // in bytes
-    char is_directory; // 1 if directory, 0 if file
+    uint8_t is_directory; // 1 if directory, 0 if file
 };
 
 struct FileHandle{
@@ -29,14 +28,14 @@ struct FileHandle{
     unsigned int position;
 };
 
-typedef struct FAT *FATEntry;
+typedef fat_entry_t *FATEntry;
 typedef struct File *FileEntry;
 typedef struct FileHandle *FileHandler;
 
 int init_fat(char* buffer);
-int find_free_block(FATEntry fat);
-int allocate_block(FATEntry fat, unsigned int start_block);
-int free_block(FATEntry fat, unsigned int block_index);
+fat_entry_t find_free_block(FATEntry fat);
+fat_entry_t allocate_block(FATEntry fat, fat_entry_t start_block);
+fat_entry_t free_block(FATEntry fat, fat_entry_t block_index);
 
 int createFile(const char* name, char* buffer);
 int eraseFile(FileEntry file, char* buffer);
